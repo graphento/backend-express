68af6022396c90752bbca3fb0b68bfe998460f95
graphento <idc>
2026-03-24 17:12:36 +0500

    5. adduser

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const users = {
+  items: [
+    {id: 1, name: 'Михайлов Даниил'},
+    {id: 2, name: 'Бочкарёва Дарья'}
+  ]
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    const users = {
-    items: [
-      {id: 1, name: 'FRONTENDER_1'},
-      {id: 2, name: 'FRONTENDER_2'}
-    ]
-  }
   res.send(users);
-});
+})
+.post('/', function(req, res, next) {
+  const id = users.items[users.items.length - 1].id + 1;
+  const newUser = {id, name: req.body.name};
+  users.items.push(newUser);
+  res.status(201).json(newUser);
+})
 
 module.exports = router;
